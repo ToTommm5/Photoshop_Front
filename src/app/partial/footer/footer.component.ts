ca0e458d0cc129bce862f7d65220ac5fe561dcf7
Tom <tom@moneret.fr>
2025-03-12 21:47:33 +0100

    Ajout du footer

---
+++ src/app/partial/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent implements OnInit {
+  ngOnInit(): void {}
+}
