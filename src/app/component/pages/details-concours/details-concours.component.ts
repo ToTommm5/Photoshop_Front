bf83f38aa2b48a975d81cd1d09b767d44fa3e3ba
Tom <tom@moneret.fr>
2025-03-20 23:00:26 +0100

    relier toutes les photos au backend

--- src/app/component/pages/details-concours/details-concours.component.ts
+++ src/app/component/pages/details-concours/details-concours.component.ts
@@ -3,10 +3,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
-import { Data } from '../../../Models/data.model';
+import { Concours, Epreuve, Photo } from '../../../Models/data.model';
 import { ConcoursService } from '../../../services/concours.service';
 import { PanierService } from '../../../services/panier.service';
-
 @Component({
   selector: 'app-details-concours',
   imports: [CommonModule, HttpClientModule],
@@ -29,30 +28,46 @@ export class DetailsConcoursComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.concoursService.getData().subscribe((data: Data) => {
-      this.concours = data.concours;
+    this.concoursId = this.route.snapshot.paramMap.get('concoursId')!;
+    this.epreuveId = this.route.snapshot.paramMap.get('epreuveId')!;
 
-      // Récupérer les IDs depuis l'URL
-      this.concoursId = this.route.snapshot.paramMap.get('concoursId')!;
-      this.epreuveId = this.route.snapshot.paramMap.get('epreuveId')!;
+    this.concoursService.getConcours().subscribe(
+      (data: any) => {
+        this.concours = data.concours;
 
-      // Récupérer les détails du concours
-      this.concoursDetails = this.concours.find(
-        (c) => c.id === this.concoursId
-      );
-
-      // Récupérer les détails de l’épreuve
-      if (this.concoursDetails) {
-        this.epreuveDetails = this.concoursDetails.epreuves.find(
-          (e: { id: string }) => e.id === this.epreuveId
+        this.concoursDetails = this.concours.find(
+          (c: Concours) => c.id === this.concoursId
         );
-        if (this.epreuveDetails) {
-          this.photos = this.epreuveDetails.photos;
-        }
-      }
-    });
-  }
 
+        if (this.concoursDetails) {
+          this.concoursDetails.img_url =
+            this.concoursDetails.img_url.startsWith('http')
+              ? this.concoursDetails.img_url
+              : `http://localhost:5273${this.concoursDetails.img_url}`;
+
+          this.epreuveDetails = this.concoursDetails.epreuves.find(
+            (e: Epreuve) => e.id === this.epreuveId
+          );
+
+          if (this.epreuveDetails) {
+            // ⚠️ Vérifier que les images ont bien un chemin complet
+            this.photos = this.epreuveDetails.photos.map((photo: Photo) => ({
+              id: photo.id,
+              img_url: photo.img_url.startsWith('http')
+                ? photo.img_url
+                : `http://localhost:5273${photo.img_url}`,
+            }));
+          }
+        }
+      },
+      (error) => {
+        console.error(
+          'Erreur lors du chargement des concours depuis l’API :',
+          error
+        );
+      }
+    );
+  }
   // Gestion de la lightbox
   lightboxOpen: boolean = false;
   currentPhotoIndex: number = 0;
--- src/app/services/concours.service.ts
+++ src/app/services/concours.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Data } from '../Models/data.model'; // Import de l'interface
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConcoursService {
+  private apiUrl = 'http://localhost:5273/api/concours'; // URL de ton API ASP.NET
+
   constructor(private http: HttpClient) {}
 
-  // Récupérer l'ensemble des données depuis un fichier JSON
-  getData(): Observable<Data> {
-    return this.http.get<Data>('Data/data.json'); // Utilisation du type Data
+  // Récupérer les concours
+  getConcours(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
   }
 }
